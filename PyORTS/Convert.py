574619c331c3c57e97d7ee32c513b479673b7d9d
TremeSignalingAndTrains <tremeempresa@gmail.com>
2020-11-28 15:10:34 +0100

    Add files via upload

---
+++ PyORTS/Convert.py
@@ -0,0 +1,23 @@
+import socket
+import serial
+import threading
+
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c.connect(('localhost', 5090))
+s = serial.Serial('COM12', 115200, timeout=0.05)
+#c.send("register(asfa::pulsador::ilum::*)\nregister(asfa::leds::*)\nregister(asfa::pantalla::iniciar)\nregister(asfa::pantalla::apagar)\nregister(asfa::sonido::iniciar)\nregister(asfa::sonido::detener)\n".encode())
+
+def SerialToTCP():
+    while True:
+        data = s.read(128)
+        c.sendall(data)
+def TCPToSerial():
+    while True:
+        data = c.recv(128,)
+        s.write(data)
+
+hilo1 = threading.Thread(target=SerialToTCP)
+hilo2 = threading.Thread(target=TCPToSerial)
+hilo1.start()
+hilo2.start()
+
