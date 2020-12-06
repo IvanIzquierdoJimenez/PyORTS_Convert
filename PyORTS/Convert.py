d12c6dfefc4ca3f8c281deec0c4d25aed54192f2
TremeSignalingAndTrains <tremeempresa@gmail.com>
2020-12-06 20:29:06 +0100

    Añadido mas funcionalidad al codigo inicial

--- PyORTS/Convert.py
+++ PyORTS/Convert.py
@@ -1,26 +1,38 @@
 import socket
 import serial
 import threading
-
-port = input("Escriba el puerto serial donde tienes conectado tu arduino o microcontrolador: ")
-c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-c.connect(('localhost', 5090))
-try:
-  s = serial.Serial(port, 115200, timeout=0.05)
-except:
-  print("El Puerto ya esta ocupado, intente otro")
+while True:
+    port = input("Escriba el puerto serial donde tienes conectado tu arduino o microcontrolador: ")
+    try:
+        c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        c.connect(('localhost', 5090))
+    except:
+        print("El servidor esta cerrado, compruebe que este conectado e intentelo de nuevo")
+    try:
+        s = serial.Serial(port, 115200, timeout=0.05)
+        if s.isOpen():
+            break;
+    except:
+        print("El Puerto ya esta ocupado, intente otro")
 
 def SerialToTCP():
     while True:
-        data = s.read(128)
-        c.sendall(data)
+        try:
+            data = s.read(128)
+            c.sendall(data)
+        except:
+            return
 def TCPToSerial():
     while True:
-        data = c.recv(128)
-        s.write(data)
-
+        try:
+            data = c.recv(128)
+            s.write(data)
+        except:
+            return
 hilo1 = threading.Thread(target=SerialToTCP)
 hilo2 = threading.Thread(target=TCPToSerial)
 hilo1.start()
 hilo2.start()
+
+
 
